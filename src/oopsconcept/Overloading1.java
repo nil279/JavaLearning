66191f5d343c1cfbf52a836d208fed75309a6f80
nil279 <nil279@gmail.com>
2018-09-21 22:28:29 -0400

    first commit

---
+++ src/oopsconcept/Overloading1.java
@@ -0,0 +1,23 @@
+package oopsconcept;
+
+public class Overloading1 {
+
+	public static void main(String args[]) {
+		System.out.println("main method");
+		Overloading1 obj = new Overloading1();
+		System.out.println(obj.add(5,7));
+		System.out.println(obj.add("nilesh","patel"));
+		System.out.println(obj.add(6,7));
+	}
+	
+	public String add(String a, String b) {
+		System.out.println("String");
+		return a+"---"+b;
+	}
+	
+	public int add(int a , int b) {
+		System.out.println("Integer ");
+		return a + b ;
+	
+	}
+}
