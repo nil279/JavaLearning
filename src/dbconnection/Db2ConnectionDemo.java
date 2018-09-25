1691d9d8c7de5d476b41141ae14b0af69aa794ac
nil279 <nil279@gmail.com>
2018-09-25 16:44:24 -0400

    25 office

--- src/dbconnection/Db2ConnectionDemo.java
+++ src/dbconnection/Db2ConnectionDemo.java
@@ -11,54 +11,50 @@ public class Db2ConnectionDemo {
 
 	public static void main(String[] args) {
 		String JDBCClassName1 = "com.ibm.db2.jcc.DB2Driver";
-		
-		String url =null;
+
+		String url = null;
 		String user = null;
 		String password = null;
-		
+
 		Connection conn = null;
 		Statement stmt = null;
-		ResultSet rs = null ;
+		ResultSet rs = null;
 		String rowcnt = null;
-		
-		// get credentials from property file : 
-		
+
+		// get credentials from property file :
+
 		Properties prop = new Properties();
-		ClassLoader loader = Thread.currentThread().getContextClassLoader();           
+		ClassLoader loader = Thread.currentThread().getContextClassLoader();
 		InputStream stream = loader.getResourceAsStream("Db2Con.properties");
 		try {
 			prop.load(stream);
 			System.out.println("Loaded property file");
-			
-					url = prop.getProperty("dburl");
-					user = prop.getProperty("dbuser");
-					password = prop.getProperty("dbpassword");
-			
-				System.out.println(url);
-			    System.out.println(user);
-			    System.out.println(password);
-			
-			
+
+			url = prop.getProperty("dburl");
+			user = prop.getProperty("dbuser");
+			password = prop.getProperty("dbpassword");
+			//String sds = (String) prop.setProperty("temp","temporary");
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		//DB2 connection 
-		
-		
-		
+		// DB2 connection
 
+		System.out.println(url);
+		System.out.println(user);
+		System.out.println(password);
 		try {
 			// Load class into memory
 			Class.forName(JDBCClassName1);
-			// Establish connection			
-   			conn = DriverManager.getConnection(url, user, password); 
-   			stmt = conn.createStatement();
-			rs = stmt.executeQuery("SELECT count(*) FROM DB2ADMIN.SYSCOVERAGECODE FETCH FOR FETCH ONLY WITH UR");  
+			// Establish connection
+			conn = DriverManager.getConnection(url, user, password);
+			stmt = conn.createStatement();
+			rs = stmt
+					.executeQuery("SELECT count(*) FROM DB2ADMIN.SYSCOVERAGECODE FETCH FOR FETCH ONLY WITH UR");
 			while (rs.next()) {
 				rowcnt = rs.getString(1);
-		        System.out.println("Total count of rows = " + rowcnt);
-		      }
+				System.out.println("Total count of rows = " + rowcnt);
+			}
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
@@ -74,6 +70,6 @@ public class Db2ConnectionDemo {
 				}
 			}
 		}
-		//System.exit(0);
-		}
+		// System.exit(0);
+	}
 }
