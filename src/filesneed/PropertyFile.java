1a756e0bf0c7e4db852c9b5ea96c677721e33837
nil279 <nil279@gmail.com>
2018-09-25 14:14:27 -0400

    25 office

---
+++ src/filesneed/PropertyFile.java
@@ -0,0 +1,36 @@
+package filesneed;
+
+import java.io.File;
+import java.util.Properties;
+
+
+/*
+ * Class to read and write to property files 
+ */
+
+public class PropertyFile {
+
+	protected Properties     props = new Properties();
+	protected File file;
+	protected String comment=null;
+	
+	public PropertyFile() 
+	{
+	}
+	
+	public PropertyFile(String Directory,String filename){
+		init(new File(Directory, filename));
+	}
+	
+	private void init(File PropertyFile){
+		try {
+
+				file = PropertyFile;
+		
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	
+
+}
