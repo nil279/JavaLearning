bdc374979a661462c8c9730c4963b7d0b19d3ff6
nil279 <nil279@gmail.com>
2018-09-28 15:49:31 -0400

    28 office

--- src/regexptest/RegexpDemo.java
+++ src/regexptest/RegexpDemo.java
@@ -11,16 +11,42 @@ public class RegexpDemo {
     public static void main(String[] args) {
 
     	Logger logger = Logger.getLogger(RegexpDemo.class);
+    	
+    	/*
+    	String product = ".*http://.*";
         String text    =
-            "This is the text to be searched " +
-            "for occurrences of the http:// pattern.";
-
+          "This is the text to be searched " +
+           "for occurrences of the http:// pattern.";
         String patternString = ".*http://.*";
+    	*/
+
+    	String text    = "EOD_abc_12345678_ABCD_12345678";
+    	String patternString = "^EOD_(.*)_[0-9]{8}_(.*)_[0-9].*";
 
         Pattern pattern = Pattern.compile(patternString);
     
         Matcher matcher = pattern.matcher(text);
         boolean matches = matcher.matches();        
+        
+        if(matches){
+        	System.out.println("String matches = " + matches);
+        }else{
+        	
+        }
+        
         logger.info(matches);
+        
+        
+        String prod = matcher.group(0);  
+        System.out.println(prod);
+                
+        String product = "EOD_abc_12345678_ABCD_12345678";
+    	
+        if (prod.equalsIgnoreCase(product)){
+        		System.out.println("matches");
+        }else{
+        	
+        	System.out.println("doesnt match");
+        }
     }
 }
