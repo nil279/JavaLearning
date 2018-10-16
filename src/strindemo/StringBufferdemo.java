8749988bff6a60896638cfe14e49111599d57bf0
npatel <npatel@Npatel_rd.centtric.com>
2018-10-16 10:27:08 -0400

    test

--- src/strindemo/StringBufferdemo.java
+++ src/strindemo/StringBufferdemo.java
@@ -1,17 +1,20 @@
 package strindemo;
+//https://stackoverflow.com/questions/355089/difference-between-stringbuilder-and-stringbuffer
 
 public class StringBufferdemo {
 	public static void main(String[] args) {
-		int N = 77777777;
+		int N = 7777777;
 		long t;
 		{
 			StringBuffer sb = new StringBuffer();
 			t = System.currentTimeMillis();
-			for (int i = N; i-- > 0;) {
+			//for (int i = N; i-- > 0;) {
+			for (int i = N; i > 0; i--){
 				sb.append("x");
 			}
 			System.out.println(System.currentTimeMillis() - t);
-		}
+			//System.out.println(sb);
+		} 
 		{
 			StringBuilder sb1 = new StringBuilder();
 			t = System.currentTimeMillis();
